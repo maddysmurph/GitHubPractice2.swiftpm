ee8ec7e1f149e4968e26245889d7a5b918624376
Neha Darji <ndarji7094@stu.d214.org>
2024-04-15 14:43:48 -0500

    Created MabLib View with noun, verb, and adjective properties

---
+++ MadLib.swift
@@ -0,0 +1,14 @@
+//
+//  MadLib.swift
+//  GitHubPractice2
+//
+//  Created by Neha Darji on 4/15/24.
+//
+
+import SwiftUI
+
+struct MadLib {
+    var noun: String = ""
+    var verb: String = ""
+    var adjective: String = ""
+}
