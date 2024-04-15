6995fc7970c36cc4f46884b876344d90346e42bc
Neha Darji <ndarji7094@stu.d214.org>
2024-04-15 14:19:33 -0500

    Created NounView and NavigationLink to VerbView

---
+++ NounView.swift
@@ -0,0 +1,16 @@
+//
+//  NounView.swift
+//  GitHubPractice2
+//
+//  Created by Neha Darji on 4/15/24.
+//
+
+import SwiftUI
+
+struct NounView: View {
+    var body: some View {
+        NavigationLink("Verb View") {
+            VerbView()
+        }
+    }
+}
