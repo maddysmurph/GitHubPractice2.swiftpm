5d5eaaead0f379534b525c56f207b331e9722382
Maddy Murphy <mmurphy6284@stu.d214.org>
2024-04-15 14:34:40 -0500

    Added SummaryView and text w/ sentence

--- NounView.swift
+++ NounView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct NounView: View {
     var body: some View {
         NavigationLink("Verb View") {
-            //VerbView()
+            VerbView()
         }
     }
 }
---
+++ SummaryView.swift
@@ -0,0 +1,15 @@
+//
+//  SummaryView.swift
+//  GitHubPractice2
+//
+//  Created by Maddy Murphy on 4/15/24.
+//
+
+import SwiftUI
+
+struct SummaryView: View {
+    var body: some View {
+        Text("The quick brown fox jumped over the lazy dog")
+        
+    }
+}
