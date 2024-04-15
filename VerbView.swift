5a143a6a2c084ff31e914a29dc84d338f102d0b8
Maddy Murphy <mmurphy6284@stu.d214.org>
2024-04-15 14:26:31 -0500

    Added Verb View and navigation link to adjective view

---
+++ VerbView.swift
@@ -0,0 +1,18 @@
+//
+//  VerbView.swift
+//  GitHubPractice2
+//
+//  Created by Maddy Murphy on 4/15/24.
+//
+
+import SwiftUI
+
+struct VerbView: View {
+    var body: some View {
+        NavigationView{
+            NavigationLink("Click Here"){
+                AdjectiveView()
+            }
+        }
+    }
+}
